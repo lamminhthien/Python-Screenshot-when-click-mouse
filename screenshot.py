c1a8d72abb5a1001217386d7e5d64f265a61d648
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-07-20 22:27:38 +0700

    k

--- screenshot.py
+++ screenshot.py
@@ -13,12 +13,13 @@ count = 0
 
 
 def click(x,y,button,pressed):
-    imageRaw = pyautogui.screenshot()
-    imageRaw = cv2.cvtColor(np.array(imageRaw),cv2.COLOR_RGB2BGR)
-    global count
-    count = count + 1
-    cv2.imwrite(imageName + str(count) + ".png", imageRaw)
-    print("Mouse is Clicked at (",x,",",y,")","with",button)
+    if pressed:
+        imageRaw = pyautogui.screenshot()
+        imageRaw = cv2.cvtColor(np.array(imageRaw),cv2.COLOR_RGB2BGR)
+        global count
+        count = count + 1
+        cv2.imwrite(imageName + str(count) + ".png", imageRaw)
+        print("Mouse is Clicked at (",x,",",y,")","with",button)
 
 with Listener(on_click=click) as listener:
     listener.join()
