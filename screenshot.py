57a18624580edccf7a4474fc1c082fa7cd72da72
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-07-20 22:31:07 +0700

    Fix duplicate picture

--- screenshot.py
+++ screenshot.py
@@ -9,17 +9,17 @@ __PIL_TUPLE_VERSION = tuple(int(x) for x in PIL.__version__.split("."))
 pyscreeze.PIL__version__ = __PIL_TUPLE_VERSION
 
 imageName=input("Please enter name for picture: ")
-count = 0
-
+quantity = 0
 
 def click(x,y,button,pressed):
     if pressed:
         imageRaw = pyautogui.screenshot()
         imageRaw = cv2.cvtColor(np.array(imageRaw),cv2.COLOR_RGB2BGR)
-        global count
-        count = count + 1
-        cv2.imwrite(imageName + str(count) + ".png", imageRaw)
-        print("Mouse is Clicked at (",x,",",y,")","with",button)
+        global quantity
+        quantity = quantity + 1
+        imageFileName = imageName + str(quantity) + ".png"
+        cv2.imwrite(imageFileName, imageRaw)
+        print("Captured image:" + imageFileName)
 
 with Listener(on_click=click) as listener:
     listener.join()
